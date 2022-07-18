40bfdaca1a336ef64a1877cb46f65b6436903872
Sanjib-123 <sanjibgiri.nalanda44@gmail.com>
2022-07-18 18:04:18 +0530

    Hibernate proj1

---
+++ HBProj01-saveObject/src/com/nt/test/SaveObjectTest.java
@@ -0,0 +1,60 @@
+package com.nt.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.nt.entity.Product;
+
+public class SaveObjectTest {
+
+	public static void main(String[] args) {
+		//activate HB framework
+		Configuration cfg = new Configuration();
+		//specify HB cfg file name and location
+		cfg.configure("/com/nt/cnfgs/hibernate.cfg.xml");
+		//create HB SessionFactory object
+		SessionFactory factory = cfg.buildSessionFactory();
+		//create session object
+		Session ses = factory.openSession();
+		//prepare entity class object having data
+		Product p = new Product();
+		p.setPid(9011);p.setPname("table");
+		p.setPrice(9000.55);p.setQty(10.0);
+		p.setStatus("available");
+		
+		//saave object
+		Transaction tx = null;
+		boolean flag = true;
+		try {
+			//begin tx
+			tx = ses.beginTransaction(); //internally calls con.setAutoCommit(false)
+			ses.save(p);
+			flag = true;
+		}//try
+		catch(HibernateException he) {
+			he.printStackTrace();
+			flag = false;
+		}//catch
+		catch(Exception e) {
+			e.printStackTrace();
+			flag = false;
+		}//catch
+		finally {
+			//perform Tx Mgmt
+			if(flag) {
+				tx.commit(); //internally calls con.commit method
+				System.out.println("Object is saved(Record is inserted");
+			}//if
+			else {
+				tx.rollback(); //Internally calls con.rollback method
+				System.out.println("Object is not saved(Record is not inserted)");
+			}//else
+		}//finally
+		
+
+	}
+
+}
