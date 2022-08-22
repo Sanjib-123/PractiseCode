00f6b7a0f24a57f0bb8fae879d01e6da16be4427
Sanjib-123 <sanjibgiri.nalanda44@gmail.com>
2022-08-22 17:15:07 +0530

    hb generator

---
+++ HBProj21-CustomIdGenerator/lib/src/main/java/multithreaded/MyRequest.java
@@ -0,0 +1,43 @@
+package multithreaded;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.nt.entity.Product;
+import com.nt.utility.HibernateUtil;
+
+public class MyRequest implements Runnable {
+	
+	@Override
+	public void run() {
+		System.out.println("Thread name::"+Thread.currentThread().getName());
+		try {
+		if(Thread.currentThread().getName().equalsIgnoreCase("thread2"))
+			Thread.sleep(40000);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		Transaction tx = null;
+		Session ses = HibernateUtil.getSession();
+		Product p1 = new Product();
+		p1.setPname("reclyner sofa");
+		p1.setPrice(55454.0);
+		p1.setQty(90.0);
+		p1.setStatus("available");
+		try(ses){
+			tx = ses.beginTransaction();
+			int idVal = (int) ses.save(p1);
+			System.out.println("id value::"+idVal);
+			tx.commit();
+		}
+		catch(HibernateException he) {
+			if(tx!=null || tx.getStatus()!=null || tx.getRollbackOnly())
+				tx.rollback();
+		}
+		
+
+	}
+
+}
